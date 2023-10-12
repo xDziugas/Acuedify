93467c4e96e27b9bf657abee0c02d7e91dfa320c
MrMnt <73601367+MrMnt@users.noreply.github.com>
2023-10-12 22:10:44 +0300

    Database-Initializer: add mock data on startup

---
+++ Data/MockDataInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Acuedify.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+
+namespace Acuedify.Data
+{
+    public class MockDataInitializer
+    {
+
+        public static void SeedQuizzes(AppDBContext context, string filePath)
+        {
+            try
+            {
+                string quizJson = File.ReadAllText(filePath);
+                var quizzes = JsonSerializer.Deserialize<List<Quiz>>(quizJson);
+                if (quizzes != null)
+                {
+                    context.AddRange(quizzes);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,8 +14,8 @@ builder.Services.AddControllersWithViews();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(
-	builder.Configuration.GetConnectionString("DefaultConnection")
-	));
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 builder.Services.AddScoped<ILibraryService, LibraryService>();
 builder.Services.AddScoped<IPlayingService, PlayingService>();
@@ -23,15 +23,15 @@ builder.Services.AddScoped<IPlayingService, PlayingService>();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromMinutes(30); // Set the session timeout as needed
-	options.Cookie.HttpOnly = true;
-	options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set the session timeout as needed
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
 });
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
-	options.CheckConsentNeeded = context => true;
-	options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
 });
 
 //builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
@@ -41,9 +41,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -56,7 +56,16 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var appDBContext = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+    if (!appDBContext.Quizzes.Any())
+    {
+        MockDataInitializer.SeedQuizzes(appDBContext, "MockData/quizzes.json");
+    }
+}
 
 app.Run();
